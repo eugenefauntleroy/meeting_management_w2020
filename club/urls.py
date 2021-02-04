82f174743be86345e4ba7434fc05b0bd8814d49e
efaunt <iwata.mich@gmail.com>
2021-02-04 07:47:30 -0800

    Detail commit

--- club/urls.py
+++ club/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('meetings/', views.meetings, name='meetings')
+    path('meetings/', views.meetings, name='meetings'),
+    path('meetingdetail/<int:id>', views.meetingdetail, name='detail'), 
 ] 
--- club/views.py
+++ club/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Meeting, MeetingMinutes, Resource, Event
+from django.urls import reverse_lazy
 # Create your views here.
 def index(request):
     return render(request, 'club/index.html')
@@ -7,3 +8,7 @@ def index(request):
 def meetings(request):
     meetingtitle = Meeting.objects.all()
     return render(request, 'club/meetings.html', {'meetingtitle': meetingtitle})
+
+def meetingdetail(request, id):
+    meeting=get_object_or_404(Meeting, pk=id) #"Meeting" is capitalized
+    return render(request, 'club/meetingdetail.html', {'meeting' : meeting})
