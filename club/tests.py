69b73c2b88cd6f61476ac007e729e5644cbe0c86
efaunt <iwata.mich@gmail.com>
2021-02-14 12:20:25 -0800

    Models test commit

--- club/tests.py
+++ club/tests.py
@@ -16,7 +16,15 @@ class MeetingTest(TestCase):
 class DateTest(TestCase):
     def setup(self):
         self.user=User(username='user1')
-        self.meeting= Meeting(meetingtitle='First', user=self.user, meetingdate=self.date('2021,1,10'), meetingtime=self.time('00:00:00'), location='Seattle', agenda='N/A')
+        self.meeting=Meeting(meetingtitle='First', user=self.user, meetingdate=self.date('2021,1,10'), meetingtime=self.time('00:00:00'), location='Seattle', agenda='N/A')
+        #self.meeting=Meeting(meetingdate=self.date('2021,1,10')
+    def test_string(self):  
+        self.assertEqual(str(self.meeting),'First')
     
-    def test_string(self):
-        self.assertEqual(str(self.meeting), 'First')
+    def test_resource(self):
+        rec = self.resource 
+        self.assertEqual(self.resource(),rec)
+
+    def test_event(self):
+        self.assertEqual(self.event)
+ 
