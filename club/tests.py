bdebca95291949cf3a0a7bc0982e089469e58bc7
efaunt <iwata.mich@gmail.com>
2021-02-26 12:43:44 -0800

    Form commit 2_26

--- club/tests.py
+++ club/tests.py
@@ -1,7 +1,8 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
-from . models import Meeting, MeetingMinutes, Resource, Event
+from .models import Meeting, MeetingMinutes, Resource, Event
 import datetime
+from .forms import meetingForm
 # Create your tests here.
 class MeetingTest(TestCase):
     def setUp(self):
@@ -27,4 +28,27 @@ class DateTest(TestCase):
 
     def test_event(self):
         self.assertEqual(self.event)
+
+class TestMeetingForm(TestCase):
+        #valid form data
+    def test_meetingform(self):
+        form=meetingForm(data={
+            'meetingtitle':'mcqueen', 
+            'meetingdate': 'Feb. 22, 2021, midnight',
+            'meetingtime': '8 a.m.', 
+            'meetinglocation': 'Seattle'
+        })
+        self.assertTrue(form.is_valid)
+
+    def test_productform_invalid(self):
+        form=meetingForm(data={            
+            'meetingtitle':'mcqueen', 
+            'meetingdate': 'Feb. 22, 2021, midnight',
+            'meetinglocation': 'Seattle'
+        })
+        self.assertFalse(form.is_valid)
+
+
+
+ 
  
